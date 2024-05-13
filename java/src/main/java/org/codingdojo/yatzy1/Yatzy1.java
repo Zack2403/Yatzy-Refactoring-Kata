4217524303544812cc27b1c6f8844ec5cba59ae1
Zakaria BOUHIA <zakaria.bouhia@zenika.com>
2024-05-13 21:02:15 +0200

    refactoring Yatzy1 code

--- java/src/main/java/org/codingdojo/yatzy1/Yatzy1.java
+++ java/src/main/java/org/codingdojo/yatzy1/Yatzy1.java
@@ -1,244 +1,151 @@
 package org.codingdojo.yatzy1;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class Yatzy1 {
 
-    public static int chance(int d1, int d2, int d3, int d4, int d5)
-    {
-        int total = 0;
-        total += d1;
-        total += d2;
-        total += d3;
-        total += d4;
-        total += d5;
-        return total;
+    private static final int YATZY_HIGH_SCORE = 50;
+    private static final int YATZY_LOW_SCORE = 0;
+    private static final int YATZY_SCORE_ZERO = 0;
+    private static final int[] SMALL_STRAIGHT_EXPECTED_RESULT = {1, 2, 3, 4, 5};
+    private static final int[] LARGE_STRAIGHT_EXPECTED_RESULT = {2, 3, 4, 5, 6};
+    private static final int SMALL_STRAIGHT_SUM = 15;
+    private static final int LARGE_STRAIGHT_SUM = 20;
+    private int[] dice;
+
+    /**
+     * @deprecated please use the version with arguments
+     */
+    @Deprecated
+    public Yatzy1() {
     }
 
-    public static int yatzy(int... dice)
-    {
-        int[] counts = new int[6];
-        for (int die : dice)
-            counts[die-1]++;
-        for (int i = 0; i != 6; i++)
-            if (counts[i] == 5)
-                return 50;
-        return 0;
-    }
-
-    public static int ones(int d1, int d2, int d3, int d4, int d5) {
-        int sum = 0;
-        if (d1 == 1) sum++;
-        if (d2 == 1) sum++;
-        if (d3 == 1) sum++;
-        if (d4 == 1) sum++;
-        if (d5 == 1) 
-            sum++;
-
-        return sum;
-    }
-
-    public static int twos(int d1, int d2, int d3, int d4, int d5) {
-        int sum = 0;
-        if (d1 == 2) sum += 2;
-        if (d2 == 2) sum += 2;
-        if (d3 == 2) sum += 2;
-        if (d4 == 2) sum += 2;
-        if (d5 == 2) sum += 2;
-        return sum;
-    }
-
-    public static int threes(int d1, int d2, int d3, int d4, int d5) {
-        int s;    
-        s = 0;
-        if (d1 == 3) s += 3;
-        if (d2 == 3) s += 3;
-        if (d3 == 3) s += 3;
-        if (d4 == 3) s += 3;
-        if (d5 == 3) s += 3;
-        return s;
-    }
-
-    protected int[] dice;
-    public Yatzy1() {}
-    public Yatzy1(int d1, int d2, int d3, int d4, int _5)
-    {
+    public Yatzy1(int die1, int die2, int die3, int die4, int die5) {
         this();
         dice = new int[5];
-        dice[0] = d1;
-        dice[1] = d2;
-        dice[2] = d3;
-        dice[3] = d4;
-        dice[4] = _5;
+        dice[0] = die1;
+        dice[1] = die2;
+        dice[2] = die3;
+        dice[3] = die4;
+        dice[4] = die5;
+
     }
 
-    public int fours()
-    {
-        int sum;    
-        sum = 0;
-        for (int at = 0; at != 5; at++) {
-            if (dice[at] == 4) {
-                sum += 4;
-            }
+    public int chance() {
+        return IntStream.of(dice)
+            .sum();
+    }
+
+    public int yatzy() {
+        return IntStream.of(this.dice)
+            .distinct()
+            .count() == 1 ? YATZY_HIGH_SCORE : YATZY_LOW_SCORE;
+    }
+
+    public int ones() {
+        return filterDiceWithValue(1)
+            .sum();
+    }
+
+    public int twos() {
+        return filterDiceWithValue(2)
+            .sum();
+    }
+
+    public int threes() {
+        return filterDiceWithValue(3)
+            .sum();
+    }
+
+    public int fours() {
+        return filterDiceWithValue(4)
+            .sum();
+    }
+
+    public int fives() {
+        return filterDiceWithValue(5)
+            .sum();
+    }
+
+    public int sixes() {
+        return filterDiceWithValue(6)
+            .sum();
+    }
+
+    public int scorePair() {
+        return computeSumByKindCount(2)
+            .max(Integer::compareTo)
+            .orElse(0);
+    }
+
+    public int twoPair() {
+        return computeSumByKindCount(2)
+            .reduce(0, Integer::sum);
+    }
+
+    public int fourOfAKind() {
+        return computeSumByKindCount(4)
+            .reduce(0, Integer::sum);
+    }
+
+    public int threeOfAKind() {
+        return computeSumByKindCount(3)
+            .reduce(YATZY_SCORE_ZERO, Integer::sum);
+    }
+
+
+    public int smallStraight() {
+        return isStraight(SMALL_STRAIGHT_EXPECTED_RESULT) ? SMALL_STRAIGHT_SUM : YATZY_SCORE_ZERO;
+    }
+
+
+    public int largeStraight() {
+        return isStraight(LARGE_STRAIGHT_EXPECTED_RESULT) ? LARGE_STRAIGHT_SUM : YATZY_SCORE_ZERO;
+    }
+
+    public int fullHouse() {
+        int threeOfKindSum = threeOfAKind();
+        int pairSum = scorePair();
+        if (threeOfKindSum != YATZY_SCORE_ZERO && pairSum != YATZY_SCORE_ZERO) {
+            return threeOfKindSum + pairSum;
         }
-        return sum;
+        return YATZY_SCORE_ZERO;
     }
 
-    public int fives()
-    {
-        int s = 0;
-        int i;
-        for (i = 0; i < dice.length; i++) 
-            if (dice[i] == 5)
-                s = s + 5;
-        return s;
+    private Stream<Integer> computeSumByKindCount(int kindCount) {
+        return filterByKindCount(kindCount)
+            .map(count -> count.getKey() * kindCount);
     }
 
-    public int sixes()
-    {
-        int sum = 0;
-        for (int at = 0; at < dice.length; at++) 
-            if (dice[at] == 6)
-                sum = sum + 6;
-        return sum;
+    private Stream<Map.Entry<Integer, Long>> filterByKindCount(int kindCount) {
+        return countDiceByValue()
+            .stream()
+            .filter(countByDice -> countByDice.getValue() >= kindCount);
     }
 
-    public int score_pair(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] counts = new int[6];
-        counts[d1-1]++;
-        counts[d2-1]++;
-        counts[d3-1]++;
-        counts[d4-1]++;
-        counts[d5-1]++;
-        int at;
-        for (at = 0; at != 6; at++)
-            if (counts[6-at-1] >= 2)
-                return (6-at)*2;
-        return 0;
+    private Set<Map.Entry<Integer, Long>> countDiceByValue() {
+        return IntStream.of(this.dice)
+            .boxed()
+            .collect(
+                Collectors.groupingBy(Function.identity(), Collectors.counting())
+            ).entrySet();
     }
 
-    public static int two_pair(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] counts = new int[6];
-        counts[d1-1]++;
-        counts[d2-1]++;
-        counts[d3-1]++;
-        counts[d4-1]++;
-        counts[d5-1]++;
-        int n = 0;
-        int score = 0;
-        for (int i = 0; i < 6; i += 1)
-            if (counts[6-i-1] >= 2) {
-                n++;
-                score += (6-i);
-            }        
-        if (n == 2)
-            return score * 2;
-        else
-            return 0;
+    private boolean isStraight(int[] expected) {
+        return Arrays.equals(IntStream.of(dice)
+            .sorted()
+            .toArray(), expected);
     }
 
-    public static int four_of_a_kind(int _1, int _2, int d3, int d4, int d5)
-    {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[_1-1]++;
-        tallies[_2-1]++;
-        tallies[d3-1]++;
-        tallies[d4-1]++;
-        tallies[d5-1]++;
-        for (int i = 0; i < 6; i++)
-            if (tallies[i] >= 4)
-                return (i+1) * 4;
-        return 0;
-    }
-
-    public static int three_of_a_kind(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] t;
-        t = new int[6];
-        t[d1-1]++;
-        t[d2-1]++;
-        t[d3-1]++;
-        t[d4-1]++;
-        t[d5-1]++;
-        for (int i = 0; i < 6; i++)
-            if (t[i] >= 3)
-                return (i+1) * 3;
-        return 0;
-    }
-
-
-    public static int smallStraight(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
-        if (tallies[0] == 1 &&
-            tallies[1] == 1 &&
-            tallies[2] == 1 &&
-            tallies[3] == 1 &&
-            tallies[4] == 1)
-            return 15;
-        return 0;
-    }
-
-    public static int largeStraight(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
-        if (tallies[1] == 1 &&
-            tallies[2] == 1 &&
-            tallies[3] == 1 &&
-            tallies[4] == 1
-            && tallies[5] == 1)
-            return 20;
-        return 0;
-    }
-
-    public static int fullHouse(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] tallies;
-        boolean _2 = false;
-        int i;
-        int _2_at = 0;
-        boolean _3 = false;
-        int _3_at = 0;
-
-
-
-
-        tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
-
-        for (i = 0; i != 6; i += 1)
-            if (tallies[i] == 2) {
-                _2 = true;
-                _2_at = i+1;
-            }
-
-        for (i = 0; i != 6; i += 1)
-            if (tallies[i] == 3) {
-                _3 = true;
-                _3_at = i+1;
-            }
-
-        if (_2 && _3)
-            return _2_at * 2 + _3_at * 3;
-        else
-            return 0;
+    private IntStream filterDiceWithValue(int value) {
+        return IntStream.of(dice)
+            .filter(die -> die == value);
     }
 }
 
